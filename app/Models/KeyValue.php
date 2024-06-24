9023b4e61e9ca580a54bd5d94d3bef064d7805d3
amypyae <ampyaephyonaing@gmail.com>
2024-06-24 14:07:17 +0800

    UPDATE KeyValue model, set $timestamps to false.

--- app/Models/KeyValue.php
+++ app/Models/KeyValue.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class KeyValue extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = ['key', 'value', 'created_at'];
 }
